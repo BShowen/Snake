d67b12eaaa6cb9ef1dc83cbf593a4182b1a7a74b
Bradley Showen <BShowen@me.com>
2022-09-28 12:57:16 -0400

    Complete initial setup of webpack

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {},
+};
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-undef */
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+module.exports = {
+  devtool: "inline-source-map",
+  entry: "./src/index.js",
+  mode: "development",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Snake",
+    }),
+    new ESLintPlugin(),
+  ],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+};
